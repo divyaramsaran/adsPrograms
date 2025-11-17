3783290069daa146ec7fb0d222a8042cdaa3fa06
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-11-17 08:04:25 +0000

    ads

---
+++ mergeSort.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class sort {
+    public void merge(int [] array, int start, int mid, int end) {
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
+
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+
+        for(int i = 0; i < n1; i++) {
+            left[i] = array[start + i];
+        }
+        for(int j = 0; j < n2; j++) {
+            right[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = start;
+        while(i < n1 && j < n2) {
+            if(left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+            
+        while(i < n1) {
+            array[k++] = left[i++];
+        }
+        while(j < n2) {
+            array[k++] = right[j++];
+        }
+    }
+
+    void sortArray(int [] array, int start, int end) {
+        if(start < end) {
+            int mid = (start + end) / 2;
+            sortArray(array, start, mid);
+            sortArray(array, mid + 1, end);
+            merge(array, start, mid, end);
+        }
+    }
+}
+
+class mergeSort {
+    public static void main(String [] args){
+        System.out.println("Enter the array length");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int array [] = new int [length];
+        System.out.println("Enter the array elements");
+
+        for(int range = 0; range < length; range++) {
+            array[range] = sc.nextInt();
+        }
+
+        sort obj = new sort();
+        obj.sortArray(array, 0, length - 1);
+
+        for(int index = 0; index < length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
---
+++ quickSort.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class sort {
+    
+    int partition (int [] array, int lower, int upper) {
+        int pivot = array[upper];
+        // System.out.println("lower" + lower);
+        int i = (lower - 1);
+        for (int j = lower; j < upper; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[upper];
+        array[upper] = temp;
+        return i + 1;
+    }
+
+    void quickSort (int [] array, int lower, int upper) {
+        if (lower < upper) {
+            int pivot = partition(array, lower, upper);
+            quickSort(array, lower, pivot - 1);
+            quickSort(array, pivot + 1, upper);
+        }
+    }
+
+    void displayArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    void sortArray(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        System.out.println("Sorted array:");
+        displayArray(array);
+    }
+
+}
+
+public class quickSort {
+    public static void main(String args []) {
+        System.out.println("Enter the length of the array");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        System.out.println("Enter the array elements");
+        int arrary [] = new int[length];
+        
+        for(int i = 0; i < length; i++) {
+            arrary[i] = sc.nextInt();
+        }
+
+        sort sorter = new sort();
+        sorter.sortArray(arrary);
+    }
+}
